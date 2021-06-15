4376aaaaebb9bfaaeb6916e05774ea72e1f88a27
Kalan Prudhomme <cleaver9@gmail.com>
2021-06-15 07:50:14 -0700

    added green and blue routes, all tests pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,15 +16,31 @@ describe('app routes', () => {
 
     const res = await request(app)
       .post('/echo');
+    
     expect(res.text).toEqual('Boo');
   });
 
   it('receives red from /red via GET', async() => {
   
-
     const res = await request(app)
       .get('/red');
 
     expect(res.text).toEqual('<h1>red</h1>');
+  });
+
+  it('receives green from /green via GET', async() => {
+
+    const res = await request(app)
+      .get('/green');
+
+    expect(res.text).toEqual('<h1>green</h1>');
+  });
+
+  it('receives blue from /blue via GET', async() => {
+
+    const res = await request(app)
+      .get('/blue');
+
+    expect(res.text).toEqual('<h1>blue</h1>');
   });
 });
