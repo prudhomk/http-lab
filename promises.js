af333de43471fac2005f03ab34c3b4a2237f8a2b
Kalan Prudhomme <cleaver9@gmail.com>
2021-06-16 17:47:48 -0700

    proper method updated, test pass

--- promises.js
+++ promises.js
@@ -10,14 +10,16 @@ const promises = net.createServer(socket => {
     const request = parseRequest(data.toString());
 
     if(request.method === 'GET' && request.path === '/index.html') {
-      socket.write(createResponse({ body: '<h1>HOORAY YOU SOLVED THE LAB CONGRATULATIONS!!!!</h1>' }));
-      return fsPromises.readFile('./public/index.html', 'utf8')
-        .then(contents => createResponse(contents));
+      fsPromises.readFile('./public/index.html')
+        .then((contents) => fsPromises.writeFile('./public/index.html', contents)
+          .then(() => fsPromises.readFile('./public/index.html', 'utf8')))
+        .then((body) => socket.end(createResponse({ body })));
         
     } else {
       socket.end(createResponse({ body: 'Not Found', status: '404 Not Found', contentType: 'text/plain' }));
     }
   });
 });
-
+    
 module.exports = promises;
+
